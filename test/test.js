01627e3cd273183b049cfa3fe0964a5d3cbcff43
mac014 <brian.19970609@gmail.com>
2017-12-01 11:17:00 +0800

    swasc

--- test/test.js
+++ test/test.js
@@ -3,17 +3,16 @@
 var expect = require('chai').expect
 
 describe('swasc', function () {
-  var twcn = require('../lib/twcn')
+  var swasc = require('../lib/swasc')
 
-  describe('', function () {
-    it('twcn.convert(ascII轉為字串)', function () {
-      expect(twcn.convert('0100011001010101010011100100001101010100010010010100111101001110轉為function')).to.equal('function轉為0100011001010101010011100100001101010100010010010100111101001110')
+  describe('字串轉為ascII', function () {
+    it('swasc.convert(字串轉為ascII)', function () {
+      expect(swasc.convert("f")).to.equal("0110 0110")
     })
   })
-  describe('簡體轉繁體測試', function () {
-    it('twcn.iconvert(写一个函数将该字串转为简体)', function () {
+  /*describe('字串轉為ascII', function () {
+    it('twcn.convert()', function () {
       expect(twcn.iconvert('function轉為0100011001010101010011100100001101010100010010010100111101001110')).to.equal('0100011001010101010011100100001101010100010010010100111101001110轉為function')
     })
-  })
-
+  })*/
 })
